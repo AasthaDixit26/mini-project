f375c7e505c1c6a61b730a1fc56532d7f279906e
AasthaDixit26 <aaasthadixit@gmail.com>
2022-11-18 19:57:33 +0530

    uploading frontend of my mini-project which is about courses available

---
+++ first.js
@@ -0,0 +1,37 @@
+// variable naming
+// var myname="aastha";
+// var _myname="aastha";
+// var my_name="aastha";
+// var $myname="aastha";
+// console.log($myname);
+// datatype and typeof(checking the datatype)
+// var name="aastha dixit";
+// console.log(name);
+// console.log(typeof(name));
+// var a=4;
+// console.log(typeof(a));
+// var b=4.56;
+// console.log(typeof(b));
+// var n=true;
+// console.log(typeof(n));
+// challenge time
+// console.log(10+"20");
+// console.log(9-"5");
+// console.log("java"+"script");
+// console.log(" "+" ");
+// console.log(" "+0);
+// console.log("aastha"-"dixit");
+// console.log(true+true);
+// console.log(true+false);
+// console.log(false+true);
+// console.log(false-true);
+// NaN===NaN;
+// Number.NaN===NaN;
+// isNaN(NaN);
+// isNaN(Number.NaN);
+// Number.isNaN(NaN);
+// console.log(NaN===NaN);
+// console.log(Number.NaN===NaN);
+// console.log(isNaN(NaN));
+// console.log(Number.isNaN(NaN));
+
